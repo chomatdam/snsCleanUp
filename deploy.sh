678387ea91c4c11163a50bcf7222ab18925c169b
chomatdam <damien.chomat@upday.com>
2018-05-19 08:35:03 +0200

    Adds a label giving edition/index to each container

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ docker build -t clean-sns .
 for index in {1..5}
 do
     while IFS= read -r edition; do
-        docker run -d -e ARN=arn:aws:sns:eu-west-1:512347748389:upday_prod_pusher_editions_$edition-$index -e AWS_ACCESS_KEY_ID=$1 -e AWS_SECRET_ACCESS_KEY=$2 clean-sns;
+        docker run --name "clean-$edition-$index" -d -e ARN=arn:aws:sns:eu-west-1:512347748389:upday_prod_pusher_editions_$edition-$index -e AWS_ACCESS_KEY_ID=$1 -e AWS_SECRET_ACCESS_KEY=$2 clean-sns;
     done <./editions.properties
 done
